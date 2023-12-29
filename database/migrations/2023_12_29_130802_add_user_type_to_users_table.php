b6abd2105502f5327275477caccae5ebeb951ba1
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-12-29 13:13:01 +0200

    user_type after password in users table

--- database/migrations/2023_12_29_130802_add_user_type_to_users_table.php
+++ database/migrations/2023_12_29_130802_add_user_type_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('user_type')->default('user'); // 'user' or 'admin' or other roles
+            $table->string('user_type')->after('password')->default('user'); // 'user' or 'admin' or other roles
         });
     }
 
